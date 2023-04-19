a95d85841a17bead1376bb5a5bd5a3bc34479989
Oliver <44035033+ollyjc99@users.noreply.github.com>
2023-04-19 14:38:29 +0100

    placed all the code within an if statement to only run for a certain record type

--- src/deliveryMOV.js
+++ src/deliveryMOV.js
@@ -1,176 +1,180 @@
 function runAction(payload) {
     const { data: { record : {Pricebook2Id}, record, related: {Account: [Account], OrderItem, Product2, PricebookEntry, aforza__Inventory__c: [Inventory]}}, data } = payload; // Deconstruct payload
-    let standardProductId, standardThreshold, standardPbe, outOfRouteProductId, outOfRouteThreshold, outOfRoutePbe;
-    const holidays = {'2023-04-07': ['All'], '2023-12-25': ['All'], '2023-12-08': ['All'], '2023-12-01': ['All'], '2023-11-01': ['All'], '2023-10-05': ['All'], '2023-08-15': ['All'], '2023-06-10': ['All'], '2023-06-08': ['All'], '2023-04-25': ['All', 'Warehouse - Alcains'], '2023-05-01': ['All'], '2023-10-22': ['Warehouse - Grândola'], '2023-05-22': ['Warehouse - Leiria'], '2023-06-13': ['Warehouse - Camarate'], '2023-05-23': ['Warehouse - Portalegre'], '2023-06-24': ['Warehouse - Porto'], '2023-06-29': ['Warehouse - Setúbal', 'Warehouse - Évora', 'Warehouse - Bombarral'], '2023-09-03': ['Warehouse - Algoz'], '2023-05-18': ['Warehouse - Beja'], '2023-07-04': ['Warehouse - Coimbra'], '2023-09-07': ['Warehouse - Faro']};
-    let orderTotal = 0;
-    let response = {orderChanged : false, reprice : false};
-    let message;
+    if(record.Order_Record_Type__c != 'Devolution'){ 
+        let standardProductId, standardThreshold, standardPbe, outOfRouteProductId, outOfRouteThreshold, outOfRoutePbe;
+        const holidays = {'2023-04-07': ['All'], '2023-12-25': ['All'], '2023-12-08': ['All'], '2023-12-01': ['All'], '2023-11-01': ['All'], '2023-10-05': ['All'], '2023-08-15': ['All'], '2023-06-10': ['All'], '2023-06-08': ['All'], '2023-04-25': ['All', 'Warehouse - Alcains'], '2023-05-01': ['All'], '2023-10-22': ['Warehouse - Grândola'], '2023-05-22': ['Warehouse - Leiria'], '2023-06-13': ['Warehouse - Camarate'], '2023-05-23': ['Warehouse - Portalegre'], '2023-06-24': ['Warehouse - Porto'], '2023-06-29': ['Warehouse - Setúbal', 'Warehouse - Évora', 'Warehouse - Bombarral'], '2023-09-03': ['Warehouse - Algoz'], '2023-05-18': ['Warehouse - Beja'], '2023-07-04': ['Warehouse - Coimbra'], '2023-09-07': ['Warehouse - Faro']};
+        let orderTotal = 0;
+        let response = {orderChanged : false, reprice : false};
+        let message;
 
-    // Get the customer segment and default to 06 (Occasional) if not found
-    let segment = Account.Customer_Segment__c;
-    if(!segment || segment == '99') {
-        segment = '06';
-    }
-    // Find the standard / out of route products and grab the ids / and minimum order value thresholds
-    Product2.forEach(getStandardAndExtraProducts);
-    PricebookEntry.forEach(getStandardAndExtraPBE);
+        // Get the customer segment and default to 06 (Occasional) if not found
+        let segment = Account.Customer_Segment__c;
+        if(!segment || segment == '99') {
+            segment = '06';
+        }
+        // Find the standard / out of route products and grab the ids / and minimum order value thresholds
+        Product2.forEach(getStandardAndExtraProducts);
+        PricebookEntry.forEach(getStandardAndExtraPBE);
 
-    // Set the threshold to 0 if it's missing
-    if(standardProductId && !standardThreshold) {
-        standardThreshold = 0;
-    }
-    if(outOfRouteProductId && !outOfRouteThreshold) {
-        outOfRouteThreshold = 0;
-    }
-    // Sum order total
-    OrderItem.forEach(sumOrderTotal);
+        // Set the threshold to 0 if it's missing
+        if(standardProductId && !standardThreshold) {
+            standardThreshold = 0;
+        }
+        if(outOfRouteProductId && !outOfRouteThreshold) {
+            outOfRouteThreshold = 0;
+        }
+        // Sum order total
+        OrderItem.forEach(sumOrderTotal);
 
-    // Decide delivery date.
-    let dt, notHoliday, dayName, manualError;
-    let deliveryDate = record.EndDate;
-    let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    let accountDeliveryDays = Account.Delivery_Day__c.split(';');
+        // Decide delivery date.
+        let dt, notHoliday, dayName, manualError;
+        let deliveryDate = record.EndDate;
+        let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+        let accountDeliveryDays = Account.Delivery_Day__c.split(';');
 
-    if(!deliveryDate) {                                                // If no Delivery Date has been selected, calculate one
-        dt = incrementDeliveryDate(new Date());                        // avoiding holidays and including account delivery days
-        dayName = days[dt.getDay()];
-        notHoliday = checkHolidays(dt) && !['Saturday', 'Sunday'].includes(dayName);
-        while (!notHoliday){
-            dt = incrementDeliveryDate(dt);
+        if(!deliveryDate) {                                                // If no Delivery Date has been selected, calculate one
+            dt = incrementDeliveryDate(new Date());                        // avoiding holidays and including account delivery days
             dayName = days[dt.getDay()];
             notHoliday = checkHolidays(dt) && !['Saturday', 'Sunday'].includes(dayName);
+            while (!notHoliday){
+                dt = incrementDeliveryDate(dt);
+                dayName = days[dt.getDay()];
+                notHoliday = checkHolidays(dt) && !['Saturday', 'Sunday'].includes(dayName);
+            }
+            record.EndDate = dt.toISOString().substring(0,10);              // Set record delivery date
+        } else {
+            dt = new Date(deliveryDate);
+            dayName = days[dt.getDay()];
+            if (!(checkHolidays(dt))){
+                manualError = true;                                         // If a Delivery Date has been selected by a rep, flag it
+            }                                                               // if it is on a public holiday or non delivery day
         }
-        record.EndDate = dt.toISOString().substring(0,10);              // Set record delivery date
-    } else {
-        dt = new Date(deliveryDate);
-        dayName = days[dt.getDay()];
-        if (!(checkHolidays(dt))){
-            manualError = true;                                         // If a Delivery Date has been selected by a rep, flag it
-        }                                                               // if it is on a public holiday or non delivery day
-    }
-    var standardDelivery = accountDeliveryDays.includes(dayName) || ["PT16", "PT17", "PT25"].includes(Account.Typology__c);
-    if(standardDelivery) {
-        removeProduct(outOfRoutePbe);
-        // Order value exceeds threshold
-        if(orderTotal >= standardThreshold) {
-            message = 'Basket Exceeds Standard Delivery Threshold, no charge';
-            removeProduct(standardPbe);
-        }
-        else {
-            message = 'Basket does not meet Standard Delivery Threshold, delivery product added';
-            putProduct(standardPbe);
-        }
-    }
-    else {
-        removeProduct(standardPbe);
-        // Order value exceeds threshold
-        if(orderTotal >= outOfRouteThreshold) {
-            message = 'Basket Exceeds Out Of Route Threshold, no charge';
+        var standardDelivery = accountDeliveryDays.includes(dayName) || ["PT16", "PT17", "PT25"].includes(Account.Typology__c);
+        if(standardDelivery) {
             removeProduct(outOfRoutePbe);
+            // Order value exceeds threshold
+            if(orderTotal >= standardThreshold) {
+                message = 'Basket Exceeds Standard Delivery Threshold, no charge';
+                removeProduct(standardPbe);
+            }
+            else {
+                message = 'Basket does not meet Standard Delivery Threshold, delivery product added';
+                putProduct(standardPbe);
+            }
         }
         else {
-            message = 'Basket does not meet Out Of Route Threshold, delivery product added';
-            putProduct(outOfRoutePbe);
+            removeProduct(standardPbe);
+            // Order value exceeds threshold
+            if(orderTotal >= outOfRouteThreshold) {
+                message = 'Basket Exceeds Out Of Route Threshold, no charge';
+                removeProduct(outOfRoutePbe);
+            }
+            else {
+                message = 'Basket does not meet Out Of Route Threshold, delivery product added';
+                putProduct(outOfRoutePbe);
+            }
         }
-    }
-    if(response.orderChanged) {
-        data.updateDeviceData = {
-            Order: true,
-            OrderItem: true
+        if(response.orderChanged) {
+            data.updateDeviceData = {
+                Order: true,
+                OrderItem: true
+            }
+            data.reprice = response.reprice;
         }
-        data.reprice = response.reprice;
-    }
-    else {
-        data.updateDeviceData = false;
-        data.reprice = false;
-    }
-    if (manualError){
-        data.message = message + `\n\nCaution: Delivery date on holiday: ${record.EndDate}\u{2757}`;
+        else {
+            data.updateDeviceData = false;
+            data.reprice = false;
+        }
+        if (manualError){
+            data.message = message + `\n\nCaution: Delivery date on holiday: ${record.EndDate}\u{2757}`;
+        } else {
+            data.message = message + `\n\nDelivery Date: ${record.EndDate}`;
+        }
+        // Function to get the Standard and OutOfRoute delivery products
+        function getStandardAndExtraProducts(product){
+            if(product.ProductCode == "000000019000000031") {
+                standardProductId = product.Id;
+                standardThreshold = product['MOV_' + segment + '__c'];
+            }
+            else if(product.ProductCode == "000000019000000034") {
+                outOfRouteProductId = product.Id;
+                outOfRouteThreshold = product['MOV_' + segment + '__c'];
+            }
+        }
+        // Function to get the Standard and OutOfRoute delivery pricebook entries
+        function getStandardAndExtraPBE(pbe){
+            if(pbe.Pricebook2Id == Pricebook2Id) {
+                if(pbe.Product2Id == standardProductId) {
+                    standardPbe = pbe;
+                    if(pbe['MOV_' + segment + '__c']){
+                        standardThreshold = pbe['MOV_' + segment + '__c'];  // Override threshold with pricebook entry
+                    }
+                }
+                else if(pbe.Product2Id == outOfRouteProductId) {
+                    outOfRoutePbe = pbe;
+                    if(pbe['MOV_' + segment + '__c']){
+                        outOfRouteThreshold = pbe['MOV_' + segment + '__c'];    // Override threshold with pricebook entry
+                    }
+                }
+            }
+        }
+        // Function to calculate the order total excluding delivery products
+        function sumOrderTotal(orderItem){
+            // Sum up total order value ignoring delivery products  
+            if(orderItem.Quantity && orderItem.UnitPrice && orderItem.aforza__Type__c != "Tax" && !(orderItem.PricebookEntryId == standardPbe.Id || orderItem.PricebookEntryId == outOfRoutePbe.Id)){
+                orderTotal += orderItem.UnitPrice * orderItem.Quantity;
+            }
+        }
+        // Function to return an incremental delivery date
+        function incrementDeliveryDate(oldDate){
+            return new Date(oldDate.setDate(oldDate.getDate() + 1));  // Will return a Date() value that is incremented by 1 day
+        }
+        // Function to check for holidays on delivery dates
+        function checkHolidays(deliveryTime){
+            dateString = deliveryTime.toISOString().substring(0,10);                             // Will return true if dateString is not in Holidays keyset or,
+            if ((Object.keys(holidays)).includes(dateString)){                                   // if it is in Holiday keyset, return true if its list contains 'All'
+                return !holidays[dateString].some(obj => ['All', Inventory.Name].includes(obj)); // or Inventory.Name, else return false
+            } else{
+                return true;
+            }
+        }
+        // Function to put Standard/OutOfRoute product in Basket
+        function putProduct(pbe) {
+            let index;
+            // Check if product already in basket
+            for(let i in OrderItem) {
+                if(OrderItem[i].PricebookEntryId == pbe.Id) {
+                    index = i;
+                    break;
+                }
+            }
+            // Add it if missing
+            if(!index) {
+                OrderItem.push({Product2Id: pbe.Product2Id, PricebookEntryId : pbe.Id, Quantity : 1, UnitPrice: 0, aforza__Type__c: "Product" });
+                response.orderChanged = true;
+                response.reprice = true;
+            }
+            // Fix quantity to 1
+            else if(OrderItem[index].Quantity != 1) {
+                OrderItem[index].Quantity = 1;
+                response.orderChanged = true;
+            }
+        }
+        function removeProduct(pbe) {
+            let index;
+            for(let i in OrderItem) {
+                if(OrderItem[i].PricebookEntryId == pbe.Id) {
+                    index = i;
+                    break;
+                }
+            }
+            if(index) {
+                OrderItem.splice(index, 1);
+                response.orderChanged = true;
+            }
+        }
     } else {
-        data.message = message + `\n\nDelivery Date: ${record.EndDate}`;
-    }
-    // Function to get the Standard and OutOfRoute delivery products
-    function getStandardAndExtraProducts(product){
-        if(product.ProductCode == "000000019000000031") {
-            standardProductId = product.Id;
-            standardThreshold = product['MOV_' + segment + '__c'];
-        }
-        else if(product.ProductCode == "000000019000000034") {
-            outOfRouteProductId = product.Id;
-            outOfRouteThreshold = product['MOV_' + segment + '__c'];
-        }
-    }
-    // Function to get the Standard and OutOfRoute delivery pricebook entries
-    function getStandardAndExtraPBE(pbe){
-        if(pbe.Pricebook2Id == Pricebook2Id) {
-            if(pbe.Product2Id == standardProductId) {
-                standardPbe = pbe;
-                if(pbe['MOV_' + segment + '__c']){
-                    standardThreshold = pbe['MOV_' + segment + '__c'];  // Override threshold with pricebook entry
-                }
-            }
-            else if(pbe.Product2Id == outOfRouteProductId) {
-                outOfRoutePbe = pbe;
-                if(pbe['MOV_' + segment + '__c']){
-                    outOfRouteThreshold = pbe['MOV_' + segment + '__c'];    // Override threshold with pricebook entry
-                }
-            }
-        }
-    }
-    // Function to calculate the order total excluding delivery products
-    function sumOrderTotal(orderItem){
-        // Sum up total order value ignoring delivery products  
-        if(orderItem.Quantity && orderItem.UnitPrice && orderItem.aforza__Type__c != "Tax" && !(orderItem.PricebookEntryId == standardPbe.Id || orderItem.PricebookEntryId == outOfRoutePbe.Id)){
-            orderTotal += orderItem.UnitPrice * orderItem.Quantity;
-        }
-    }
-    // Function to return an incremental delivery date
-    function incrementDeliveryDate(oldDate){
-        return new Date(oldDate.setDate(oldDate.getDate() + 1));  // Will return a Date() value that is incremented by 1 day
-    }
-    // Function to check for holidays on delivery dates
-    function checkHolidays(deliveryTime){
-        dateString = deliveryTime.toISOString().substring(0,10);                             // Will return true if dateString is not in Holidays keyset or,
-        if ((Object.keys(holidays)).includes(dateString)){                                   // if it is in Holiday keyset, return true if its list contains 'All'
-            return !holidays[dateString].some(obj => ['All', Inventory.Name].includes(obj)); // or Inventory.Name, else return false
-        } else{
-            return true;
-        }
-    }
-    // Function to put Standard/OutOfRoute product in Basket
-    function putProduct(pbe) {
-        let index;
-        // Check if product already in basket
-        for(let i in OrderItem) {
-            if(OrderItem[i].PricebookEntryId == pbe.Id) {
-                index = i;
-                break;
-            }
-        }
-        // Add it if missing
-        if(!index) {
-            OrderItem.push({Product2Id: pbe.Product2Id, PricebookEntryId : pbe.Id, Quantity : 1, UnitPrice: 0, aforza__Type__c: "Product" });
-            response.orderChanged = true;
-            response.reprice = true;
-        }
-        // Fix quantity to 1
-        else if(OrderItem[index].Quantity != 1) {
-            OrderItem[index].Quantity = 1;
-            response.orderChanged = true;
-        }
-    }
-    function removeProduct(pbe) {
-        let index;
-        for(let i in OrderItem) {
-            if(OrderItem[i].PricebookEntryId == pbe.Id) {
-                index = i;
-                break;
-            }
-        }
-        if(index) {
-            OrderItem.splice(index, 1);
-            response.orderChanged = true;
-        }
+        data.message = 'No MOV for Devolution order type';
     }
     return payload;
 }
