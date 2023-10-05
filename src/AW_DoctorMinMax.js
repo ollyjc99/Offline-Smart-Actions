6f9d5e83ba382a233fcb06cf3450f5adc64eb12a
Oliver <44035033+ollyjc99@users.noreply.github.com>
2023-10-05 10:48:43 +0100

    renaming javascript

--- src/AW_DoctorMinMax.js
+++ src/AW_DoctorMinMax.js
@@ -11,15 +11,22 @@ function runAction(payload) {
     const productIds = new Set(OrderItem.map(obj => obj.Product2Id));
 
     const productIdToProduct = new Map();
-    const productIdToAsset = new Map();
 
     // Get a set of ProductIds
     Product2.forEach(obj => {
-        if (productIds.has(obj.Id)){
+        if (productIds.has(obj.Id) && obj.AW_Is_Restricted_Drug__c === true){
             productIdToProduct.set(obj.Id, obj); 
         }
     });
-    // Build a map of ProductIds to Outlet Assets
+
+    if (!productIdToProduct.size){
+        return payload;
+    }
+
+    // Build a map of ProductIds to Outlet Assets and Map of Adjusted Items
+    const productIdToAsset = new Map();
+    const adjustedItems = new Array();
+
     aforza__Outlet_Asset__c.forEach(asset => {
         if (asset.aforza__Account__c === record.AccountId && productIds.has(asset.aforza__Product__c)){
             productIdToAsset.set(asset.aforza__Product__c, asset); 
@@ -34,36 +41,38 @@ function runAction(payload) {
         }
         data.reprice = true;
 
-        payload.data.message = "The quantities of some products have been adjusted to comply with yearly limits.";
-    }
-    
-    function evaluateYearlyLimit(item){
-        const product = productIdToProduct.get(item.Product2Id);
-        
-        // Get the yearly limit of the related Product using on the Account country field
-        const yearlyLimit = product["AW_Doctor_"+ account.AW_Country];
+        payload.data.message = "The quantities of some products have been adjusted to comply with yearly limits.\n\n";
 
-        //
+        // List adjusted items
+        adjustedItems.forEach(listItems);
+        
+    }
+
+    function evaluateYearlyLimit(item){
+
+        const product = productIdToProduct.get(item.Product2Id);
         const asset = productIdToAsset.get(item.Product2Id);
 
-        // Calculate the total quantity of the SKU ordered this year
-        let totalQuantityThisYear = asset.AW_Yearly_Quantity;
-
-        let quantityInOrder = item.Quantity;
-        if (totalQuantityThisYear + quantityInOrder > yearlyLimit) {
-            // Calculate the adjusted quantity
-            let adjustedQuantity = yearlyLimit - totalQuantityThisYear;
-            if (adjustedQuantity < 0) {
-                adjustedQuantity = 0;
-            }
-            // Adjust the quantity in the order and set the flag indicating we modified the order
-            item.Quantity = adjustedQuantity;
-            totalQuantityThisYear += adjustedQuantity;
-
-            orderChanged = true;
-        } else {
-            totalQuantityThisYear += quantityInOrder;
+        const yearlyLimit = product["AW_Doctor_"+account.AW_Country+"__c"];
+        const availableQuantity = yearlyLimit - asset.AW_Yearly_Quantity__c;
+        
+        // If there is enough available quantity skip this item
+        if (availableQuantity >= item.Quantity){
+            return;
         }
+
+        const oldQuantity = item.Quantity;
+
+        item.Quantity = availableQuantity <= 0 ? 0 : availableQuantity;
+
+        adjustedItems.push({"Name": product.Name, "Difference": item.Quantity - oldQuantity});
+
+        orderChanged = true;
+        return;
     }
+    function listItems(item){
+        data.message += `\{u2713} ${item.Name} ${item.Difference}`;
+    }
+
     return payload;
 }
