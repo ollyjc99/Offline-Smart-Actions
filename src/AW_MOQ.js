c003b3eb4961cfe38d4ebe30c40346cde8314308
Oliver <44035033+ollyjc99@users.noreply.github.com>
2023-10-05 13:22:14 +0100

    refactoring smart action

--- src/AW_MOQ.js
+++ src/AW_MOQ.js
@@ -5,37 +5,37 @@ function runAction(payload) {
     let productIdToProduct = new Map();
     let minProducts = new Array();
     let maxProducts = new Array();
-    if (Type == 'Product Order' &&
-        // RecordType Developer Names cannot be accessed via payload so have hard-coded Ids for each environment
-        // Checks for if the RecordType Id is equal to Medical Professionals or Pharmacies
-        (
-            Account.RecordTypeId === 'TBD' ||  // Production
-            Account.RecordTypeId === 'TBD' ||   // SIT
-            (Account.RecordTypeId === '0123L000000RQhRQAW' || Account.RecordTypeId === '0123L000000RQhQQAW')      // UAT Pharmacy & Med Prof
-        ))
-        {
-            OrderItem.forEach(obj => {
-                productIds.add(obj.Product2Id);             // Get Product2Id for each OrderItem
-            });
-            Product2.forEach(prod => {
-                if (productIds.has(prod.Id)){
-                    productIdToProduct.set(prod.Id, prod);  // Build the Map of Product.Id to Product
-                }
-            });
-            if (productIdToProduct.size){
-                OrderItem.forEach(getInvalidQuantities);    // Loop through OrderItems after constructing Map
-            }
-            if(minProducts.length || maxProducts.length) {
-                data.error = '';
-                buildError(minProducts, maxProducts);       // If there are invalid quantities, run start creating the error message
-                data.blockExecution = true;
-            } else {
-            data.message = `Bestellung best\{u00E4}tigt \{u2713}`;
-            data.updateDeviceData = true;                    // If no error, return 
-            data.reprice = true;
-            data.blockExecution = false;
-        }
+    // RecordType Developer Names cannot be accessed via payload so have hard-coded Ids for each environment
+    // Checks for if the RecordType Id is equal to Medical Professionals or Pharmacies
+    if (Type != 'Product Order' && (Account.RecordTypeId !== '0123L000000RQhRQAW' || Account.RecordTypeId !== '0123L000000RQhQQAW')){
+        return;
     }
+
+    OrderItem.forEach(obj => {
+        productIds.add(obj.Product2Id);             // Get Product2Id for each OrderItem
+    });
+
+    Product2.forEach(prod => {
+        if (productIds.has(prod.Id)){
+            productIdToProduct.set(prod.Id, prod);  // Build the Map of Product.Id to Product
+        }
+    });
+
+    if (productIdToProduct.size){
+        OrderItem.forEach(getInvalidQuantities);    // Loop through OrderItems after constructing Map
+    }
+
+    if (minProducts.length || maxProducts.length) {
+        data.error = '';
+        buildError(minProducts, maxProducts);       // If there are invalid quantities, run start creating the error message
+        data.blockExecution = true;
+    } else {
+        data.message = `Bestellung best\{u00E4}tigt \{u2713}`;
+        data.updateDeviceData = true;                    // If no error, return 
+        data.reprice = true;
+        data.blockExecution = false;
+    }
+    
     // Function to populate the lists of products that exceed the minimum and maximimum order quantities. 
     function getInvalidQuantities(item){
         if (!new Set(['Tax', 'Promotion', 'Discount']).has(item.aforza__Type__c)){
@@ -51,6 +51,7 @@ function runAction(payload) {
             }
         }
     }
+
     function listItems(item){
         data.error += `\u2717 ${item.Name} ${item.Difference}`;
     }
@@ -64,7 +65,7 @@ function runAction(payload) {
             if (data.error.length > 0){
                 spacer = '\n\n';
             }
-            data.error += `${spacer}Die folgenden Produkte überschreiten die Höchstmenge:`;
+            data.error += `${spacer}Die folgenden Produkte \{u00FC}berschreiten die H\{u00F6}chstmenge:`;
             maxProducts.forEach(listItems);
         }
     }
