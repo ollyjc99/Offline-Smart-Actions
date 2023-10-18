796947c9984c754a87390ce7be72d0ad6607375f
Oliver <44035033+ollyjc99@users.noreply.github.com>
2023-10-18 10:29:57 +0100

    new restructuring of old distell/heineken smart action

--- src/invProductValidation.js
+++ src/invProductValidation.js
@@ -1,25 +1,40 @@
 function runAction(payload) {
-  const { data: { record : {aforza__Inventory__c}, related: { OrderItem, Product2, aforza__Inventory_Product__c}}, data } = payload; // Deconstruct payload
+  // Deconstruct payload
+  const { data: { record : {aforza__Inventory__c}, related: { OrderItem, Product2, aforza__Inventory_Product__c}}, data } = payload; 
   
-  const inventoryProducts = aforza__Inventory_Product__c.filter(obj => obj.aforza__Inventory__c === aforza__Inventory__c); // Filter inventory products for selected inventory
-  const inventoryIds = new Set(inventoryProducts.map(obj => obj.aforza__Product__c));  // Create a set of product ids from inventory products
+  // Filter inventory products for selected inventory
+  const inventoryProducts = aforza__Inventory_Product__c.filter(obj => obj.aforza__Inventory__c === aforza__Inventory__c);
+
+  // Create a set of product ids from inventory products
+  const inventoryIds = new Set(inventoryProducts.map(obj => obj.aforza__Product__c)); 
   
-  const invalidOrderItems = OrderItem.filter(obj => !inventoryIds.has(obj.Product2Id)); // Filter order products to those that don't have a corresponding inventory product
-  const invalidIds = new Set(invalidOrderItems.map(obj => obj.Product2Id));  // Create an array of product ids from order products
+  // Filter order products to those that don't have a corresponding inventory product
+  const invalidOrderItems = OrderItem.filter(obj => !inventoryIds.has(obj.Product2Id));
+
+  // Create an array of product ids from order products
+  const invalidIds = new Set(invalidOrderItems.map(obj => obj.Product2Id));
   
-  const invalidProducts = Product2.filter((obj) => invalidIds.has(obj.Id) && !new Set(['Tax', 'Promotion', 'Discount']).has(obj.Name)); // Filter products with the array of invalid ids excluding Tax, Promotion and Discount
+  // Filter products with the array of invalid ids excluding Tax, Promotion and Discount
+  const invalidProducts = Product2.filter((obj) => invalidIds.has(obj.Id) && !new Set(['Tax', 'Promotion', 'Discount']).has(obj.Name));
   
+  // For each invalid product, add product name to error message with an escape sequence character
   function buildError(item){
-    data.error += `\n✗ ${item.Name}`;   // For each invalid product, add product name to error message with an escape sequence character
+    data.error += `\n✗ ${item.Name}`;  
   }
-  if (invalidProducts.length){   // If invalidProducts is not empty, block progression within order capture
-    data.error = `Invalid Products for\nSelected Plant:\n`; // Creates the opening header for return error message
-    invalidProducts.forEach(buildError);  // Build return error message with invalid products, function is not executed if list is empty
+  // If invalidProducts is not empty, block progression within order capture
+  if (invalidProducts.length){  
+     // Creates the opening header for return error message
+    data.error = `Invalid Products for\nSelected Plant:\n`;
+
+    // Build return error message with invalid products, function is not executed if list is empty
+    invalidProducts.forEach(buildError); 
     data.blockExecution = true;
     data.updateDeviceData = false;
     data.reprice = false;
-  } else{
-    data.message = '✓ Order validated';  // If invalidProducts is empty, proceed with order completion
+  } 
+  // If invalidProducts is empty, proceed with order completion
+  else{
+    data.message = '✓ Order validated';  
   }
   return payload;
 }
