cabbc697b4969b113ef738d388dadeb5caaa3426
Oliver <44035033+ollyjc99@users.noreply.github.com>
2023-10-21 19:58:59 +0100

    small changes to code format

--- src/AW_MOV.js
+++ src/AW_MOV.js
@@ -1,20 +1,24 @@
 // @author: Oliver Carter
 function runAction(payload) {
     const {data :{record, related: {Account: [Account]}}, data} = payload;
-
-    if (/*
-        Check the minimum order amount isn't null or 0 
-        Check that order total isn't 0
-        Check if the minimum order total is greater than order total
-        */
+    /*
+    Check the minimum order amount isn't null or 0 
+    Check that order total isn't 0
+    Check if the minimum order total is greater than order total
+    */
+    if (
         record.Type == 'Product Order' &&
-        (Account.aforza__Minimum_Order_Amount__c != null || Account.aforza__Minimum_Order_Amount__c != 0) 
-        && record.TotalAmount != 0                                                                          
-        && Account.aforza__Minimum_Order_Amount__c > record.TotalAmount                                     
+        (Account.aforza__Minimum_Order_Amount__c != null || Account.aforza__Minimum_Order_Amount__c != 0) &&
+        record.TotalAmount != 0 &&                                                                   
+        Account.aforza__Minimum_Order_Amount__c > record.TotalAmount                                     
         ){
-        data.error = `Die Bestellsumme liegt um weniger als der Mindestbestellwert: ${Math.round((Account.aforza__Minimum_Order_Amount__c - record.TotalAmount) * 100) / 100}`;  // Return the difference rounded to 2DP
+        // Return the difference rounded to 2DP 
+        let diff = Math.round((Account.aforza__Minimum_Order_Amount__c - record.TotalAmount) * 100) / 100;
+
+        data.error = `Die Bestellsumme liegt um weniger als der Mindestbestellwert: ${diff}`;
     } else {
-    data.message = `Bestellung best\{u00E4}tigt \{u2713}`; // Else, Order Validated
+        // Else, Order Validated
+        data.message = `Bestellung best\{u00E4}tigt \{u2713}`;
     }
     return payload;
 }
