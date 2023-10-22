77332e30a17fb8cbf4c97c9de204f54d2ae05ba5
Oliver <44035033+ollyjc99@users.noreply.github.com>
2023-10-22 01:52:18 +0100

    more minor changes

--- src/AW_ValidateBOMs.js
+++ src/AW_ValidateBOMs.js
@@ -3,28 +3,19 @@ function runAction(payload) {
 
     const bomMap = new Map();
     const productMap = new Map();
-    const childIds = new Set();
-    const bomIds = new Set();
+    const productIds = new Set();
 
     const bomOrderItems = OrderItem.filter(obj => obj.aforza__Type__c == 'BOM');
     const itemIds = new Set(bomOrderItems.map(obj => obj.Product2Id));
 
-    // Product2.forEach(obj => {
-    //     if (itemIds.has(obj.Id) && !new Set(['Tax', 'Promotion', 'Discount']).has(obj.Name) && obj.aforza__Type__c == 'BOM'){
-    //       // bomMap.set(obj.Id, []);
-    //       bomIds.add(obj.Id);
-    //       productMap.set(obj.Id, obj);
-    //     }
-    // });
-
     aforza__Relationship_Rule__c.filter(obj => itemIds.has(obj.aforza__Target_Product__c));
 
     aforza__Relationship_Rule__c.forEach(obj => {
-        childIds.add(obj.aforza__Source_Product__c);
+      productIds.add(obj.aforza__Source_Product__c);
     });
 
     Product2.forEach(obj => {
-      if (childIds.has(obj.Id) || itemIds.has(obj.Id)){
+      if (productIds.has(obj.Id) || itemIds.has(obj.Id)){
           productMap.set(obj.Id, obj);
       }
     });
@@ -35,10 +26,10 @@ function runAction(payload) {
         let bom = productMap.get(obj.aforza__Target_Product__c);
 
         if (!bomMap.has(product.Name)){
-          bomMap.set(product.Name, [bom.Name]);
+          bomMap.set(product.Name, [' ' + bom.Name]);
         }
         else {
-          bomMap.get(product.Name).push(bom.Name);
+          bomMap.get(product.Name).push(' '+ bom.Name);
         }
     });
 
@@ -51,13 +42,14 @@ function runAction(payload) {
     }
 
     if (duplicateProducts){
+      data.blockExecution = true;
       data.error = 'There are duplicate products in BOMs:\n';
 
-      duplicateProducts.forEach((boms, product) => {
-
-        data.error += ` - ${product}: ${boms}\n          `;
-      });
+      for (const [product, boms] of duplicateProducts) {
+        data.error += ` - ${product}:${boms}\n  `;
+      }
     }
+    
     else {
       data.message = 'BOMs Valid';
     }
