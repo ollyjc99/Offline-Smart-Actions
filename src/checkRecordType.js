63cf168b5677c50ed935568361ec2172b1039765
Oliver <44035033+ollyjc99@users.noreply.github.com>
2023-10-30 19:30:01 +0000

    fixing up record type conditions

--- src/checkRecordType.js
+++ src/checkRecordType.js
@@ -1,22 +1,14 @@
 function runAction(payload) {
     const {data :{related: {Account : [{RecordTypeId}]}}, data} = payload;
 
-    switch (RecordTypeId){
-         // Cannot create Order for Account with Record Type: Legal Entity'
-        case '0125I000000HZk6QAG':
-            data.error = 'Não é possível criar Pedido para Conta com Tipo: Entidade Jurídica ❌';
-            data.blockExecution = true;
-
-         // Cannot create Order for Account with Record Type: Prospect'
-        case '0125I000000HZk8QAG':
-            data.error = 'Não é possível criar Pedido para Conta com Tipo: Cliente Potencial ❌';
-            data.blockExecution = true;
-
-        default:
-            // Account Record Type Valid
-            data.message = `Tipo de conta válido ✔️`;
-            data.blockExecution = false;
-
+    if (RecordTypeId != '0125I000000HZk7QAG'){
+        data.error = 'Não é possível criar Pedido para Conta com Tipo ❌';
+        data.blockExecution = true;
+    }
+    else {
+        // Account Record Type Valid
+        data.message = `Tipo de conta válido ✔️`;
+        data.blockExecution = false;
     }
     return payload;
 }
